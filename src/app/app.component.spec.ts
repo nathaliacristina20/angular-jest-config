cd8599aba5f3b84c3affca2664503319e7f85632
Nathalia Cristina <nathaliacristina20@hotmail.com>
2020-09-20 22:39:23 -0300

    test: create first test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,22 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { render, screen } from "@testing-library/angular";
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+describe("AppComponent", () => {
+  it("should do something true", async () => {
+    await render(AppComponent, {
+      componentProperties: { name: "cristina" },
+    });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(screen.getByTestId("count")).toBeTruthy();
+    expect(screen.getByText(/cristina/i).textContent).toEqual("cristina");
   });
 
-  it(`should have as title 'angular-jest-config'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-jest-config');
-  });
+  it("should do something false", async () => {
+    await render(AppComponent, {
+      componentProperties: { name: "cristinaa" },
+    });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-jest-config!');
+    expect(screen.queryByText("cristinaa")).not.toBeInTheDocument();
+    expect(screen.queryByText("cristina")).not.toBeInTheDocument();
   });
 });
